ce5eba24b35bd8bb53948d46c6b0a3cea401bb33
hunnble <hante_hunter@163.com>
2019-04-23 00:42:14 +0800

    feat: init

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: 'Maid',
+      home: HomeScreen(),
+    );
+  }
+}
+
+const devicesTabName = '设备';
+const statisticTabName = '统计';
+const settingTabName = '设置';
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.gear_solid),
+            title: Text(devicesTabName)
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.folder_solid),
+            title: Text(statisticTabName)
+          ),
+        ]
+      ),
+      tabBuilder: (context, position) {
+        return CupertinoTabView(
+          builder: (context) {
+            return CupertinoPageScaffold(
+              navigationBar: CupertinoNavigationBar(
+                middle: position == 0 ? Text(devicesTabName) : Text(statisticTabName),
+              ),
+              child: position == 0 ? new Devices(position.toString()) : Text(statisticTabName),
+            );
+          },
+        );
+      }
+    );
+  }
+}
+
+/// devices page
+class Devices extends StatelessWidget{
+  final String position;
+  Devices(this.position);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("欢迎来到 :$position"),
+    );
+  }
+}
